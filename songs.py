a0aca454d9bf95ba6b46f332261be2a8ad0da6cd
Kykynusak <57krystof@seznam.cz>
2025-05-08 01:06:23 +0200

    mark_0

---
+++ songs.py
@@ -0,0 +1,5 @@
+{
+    "verbunk": [],
+    "vrtena": [],
+    "lidovky": []
+}
---
+++ static/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/', 
+                '/static/manifest.json',
+                '/static/icon.png', 
+                '/index.html', 
+                // přidej všechny další soubory, které chceš mít v cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
